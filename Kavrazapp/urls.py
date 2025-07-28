b724a350a762ce6bccd0873d6d7b340bb450f5f7
LoLChC <lolchc55@gmail.com>
2025-07-29 00:17:18 +0300

    asdfas

--- Kavrazapp/urls.py
+++ Kavrazapp/urls.py
@@ -21,12 +21,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-
 urlpatterns = [ 
     path('', include('Kavraz.urls')),
     path('search/', search_results, name='search'),
     path('products/', include('products.urls')),
     path("account/", include('account.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
