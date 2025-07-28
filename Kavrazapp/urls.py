8abfffa3f7ddc767db3ec427130c04009c01f0ca
LoLChC <lolchc55@gmail.com>
2025-07-29 00:21:45 +0300

    urls.py

--- Kavrazapp/urls.py
+++ Kavrazapp/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path("account/", include('account.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
